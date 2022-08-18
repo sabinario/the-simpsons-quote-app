809a27ac4ae6fc5ec435608b156ffb70f9df0a76
Sabino Fernandez <sabinofernandezr@gmail.com>
2022-08-17 23:23:26 -0500

    testing cors with api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
+import logo from './logo.svg';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [quotes, setQuotes] = useState([]);
+
+	const fetchQuotes = async () => {
+		try {
+			const response = await fetch(process.env.REACT_APP_API_URL);
+			const data = await response.json();
+			setQuotes(data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	useEffect(() => {
+		fetchQuotes();
+	}, []);
+
+	console.log(quotes);
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
